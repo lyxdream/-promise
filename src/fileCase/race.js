4fce8b66daa8ad5007a02ec93e1b095811e54b70
lyxdream <lyxdream@qq.com>
2020-12-11 00:06:56 +0800

    race

---
+++ src/fileCase/race.js
@@ -0,0 +1,69 @@
+
+
+// let p1 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve(1)
+//     },1000)
+// })
+
+// let p2 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         reject('fail')
+//     },500)
+// })
+
+// //谁先执行完毕就用谁的结果，那个率先改变的 Promise 实例的返回值，就传递给p的回调函数。
+// Promise.race([p1,p2]).then(data=>{
+//     console.log(data)
+// }).catch(e=>{
+//     console.log(e)
+// })
+
+//Promise.race能解决什么问题，不要这个promise的返回结果了，超时，中断
+
+// let abort;
+// let promise = new Promise((resolve,reject)=>{
+//     abort = reject;
+//     setTimeout(()=>{
+//         resolve('ok')
+//     },3000)
+// })
+
+// setTimeout(()=>{
+//     abort('超时')
+// },2000)
+
+// promise.catch(err=>{
+//     console.log(err)
+// })
+
+//////////////////////////////////////
+
+
+let promise = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        // console.log('111111')
+        resolve('ok')
+    },1000)
+})
+function wrap(p){
+    let abort;
+    let p2 = new Promise((resolve,reject)=>{
+       abort = reject;
+    })
+    let p3 = Promise.race([p,p2]);//能控制p的状态，主要是利用p2的reject
+    //谁先执行完毕就采用谁的结果，p3就采用谁的结果，虽然没采用p的结果，但是p还是会执行
+    p3.abort = abort;
+    return p3;
+}
+
+let p = wrap(promise);
+setTimeout(()=>{
+    p.abort('超时')  //如果超时，就会走p2的reject，则就不会采用wrap(p)的p的结果
+},500)
+
+p.then((data)=>{
+    console.log(data)
+}).catch(err=>{
+    console.log(err,'fail')
+})
